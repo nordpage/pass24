504325ef2e48927854f0b9ef3b7695d769774b26
akhorevich <a.khorevich@gmail.com>
2019-11-14 00:09:53 +0300

    Update

--- app/src/main/java/ru/nortti/numbercheck/MainActivity.kt
+++ app/src/main/java/ru/nortti/numbercheck/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,9 +19,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "MainActivity"
 
-        val RUS = Regex("^[АВЕКМНОРСТУХ]{1}[\\d]{3}[АВЕКМНОРСТУХ]{2}[\\d]{2}")
-        val TAXI = Regex("^[АВЕКМНОРСТУХ+]{2}[\\d]{5,6}")
-        val POLICE = Regex("^[АВЕКМНОРСТУХ]{1}[\\d]{6,7}")
+        val RUS = Regex("^[АВЕКМНОРСТУХ][\\d]{3}[АВЕКМНОРСТУХ]{2}")
+        val TAXI = Regex("^[АВЕКМНОРСТУХ]{2}[\\d]{3}")
+        val POLICE = Regex("^[АВЕКМНОРСТУХ][\\d]{4}")
 
         var lastChar = ""
     }
@@ -28,35 +29,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         numberET.addTextChangedListener(object : TextWatcher{
+            var space = 0
+
             override fun afterTextChanged(s: Editable?) {
-
-
 
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                numberET.setBackgroundColor(Color.TRANSPARENT)
+                numberET.requestFocus()
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                var name = ""
-                var space = 0
-                 if (POLICE.containsMatchIn(s.toString())) {
-                    space = 5
-                     if (count == space) numberET.append(" ")
-                     numberET.setBackgroundColor(Color.BLUE)
-                } else if(RUS.containsMatchIn(s.toString())) {
-                    space = 6
-                     if (count == space) numberET.append(" ")
-                     numberET.setBackgroundColor(Color.TRANSPARENT)
-                } else if (TAXI.containsMatchIn(s.toString())) {
-                     numberET.setBackgroundColor(Color.YELLOW)
-                     space = 5
-                     if (count == space) numberET.append(" ")
+                var color = 0
+                var icon = 0
+                when {
+                    POLICE.containsMatchIn(s.toString()) -> {
+                        space = 5
+                        color = Color.BLUE
+                        icon = R.drawable.police
+                    }
+                    RUS.containsMatchIn(s.toString()) -> {
+                        space = 5
+                        color = Color.GREEN
+                        icon = R.drawable.plate
+                    }
+                    TAXI.containsMatchIn(s.toString()) -> {
+                        space = 5
+                        color = Color.rgb(255, 204,0)
+                        icon = R.drawable.taxi
+                    }
+                    else -> {
+                        color = Color.BLACK
+                    }
                 }
 
+                numberET.setTextColor(color)
+                iconIV.setImageResource(icon)
                 }
 
         })
